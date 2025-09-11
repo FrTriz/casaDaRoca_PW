a9f918be53ce8e7ad81a6824ecac26b021dc5621
JosephBM7 <johbr85@gmail.com>
2025-09-11 19:47:06 -0300

    Feat: fiz alterações para se comunicar com o banco de dados em numvem da Neon.

--- php/conexao.php
+++ php/conexao.php
@@ -1,12 +1,23 @@
 <?php
-try {
-    $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=TrabalhoPDS", "postgres", "172834");
+$host = 'ep-young-unit-ac3td02j-pooler.sa-east-1.aws.neon.tech';
+$dbname = 'neondb';
+$user = 'neondb_owner';
+$pass = 'npg_3S6uYVtveZiT'; 
+$port = '5432';
+$endpoint_id = 'ep-young-unit-ac3td02j-pooler';
 
+$pass_com_endpoint = "endpoint=$endpoint_id;" . $pass;
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass_com_endpoint, $options);
 } catch (PDOException $e) {
-    echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
-    exit();
-} catch (Exception $e) {
-    echo "Erro genérico: " . $e->getMessage();
-    exit();
+    die("FALHA NA CONEXÃO (com workaround): " . $e->getMessage());
 }
 ?>
