56897b5d76d56a673a0953d7244755d6de45d84a
JosephBM7 <johbr85@gmail.com>
2025-09-23 22:20:39 -0300

    Feat: Adicionei uma linha com que o banco de dados reconheça diferente tipos de informações como "8",'8' e 8.

--- php/conexao.php
+++ php/conexao.php
@@ -2,7 +2,7 @@
 $host = 'ep-young-unit-ac3td02j-pooler.sa-east-1.aws.neon.tech';
 $dbname = 'neondb';
 $user = 'neondb_owner';
-$pass = 'npg_3S6uYVtveZiT'; 
+$pass = 'npg_3S6uYVtveZiT';
 $port = '5432';
 $endpoint_id = 'ep-young-unit-ac3td02j-pooler';
 
@@ -13,6 +13,7 @@ $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false, 
 ];
 
 try {
