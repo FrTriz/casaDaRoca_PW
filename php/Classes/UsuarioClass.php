d62384ad9849632778ccde55569da2583b3c19a3
JosephBM7 <johbr85@gmail.com>
2025-09-25 04:10:29 -0300

    Feat: Precisei alterar e adicionar alguma funções para o funcionamento da aplicação, o logout esta funcionando e é possivel atualizar o email.

--- php/Classes/UsuarioClass.php
+++ php/Classes/UsuarioClass.php
@@ -6,22 +6,31 @@ class Usuario {
         $this->pdo = $pdo;
     }
 
-    public function buscarDados($id_usuario) {
-        $sql = "SELECT c.nome, u.email, u.tipo FROM usuario u JOIN cliente c ON u.id_usuario = c.id_cliente WHERE u.id_usuario = :id_usuario";
+    public function buscarPorId($id_usuario) {
+        $sql = "SELECT id_usuario, email, tipo FROM usuario WHERE id_usuario = :id_usuario";
         $cmd = $this->pdo->prepare($sql);
-        $cmd->bindParam(':id_usuario', $id_usuario);
+        $cmd->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
         $cmd->execute();
-
         return $cmd->fetch(PDO::FETCH_ASSOC);
     }
 
+ 
+    public function atualizarEmail($id_usuario, $email) {
+        $sql = "SELECT COUNT(*) FROM Usuario WHERE email = :email AND id_usuario != :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':id', $id_usuario, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchColumn() > 0;
+    }
+    
+  
     public function logOut() {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
         session_unset();
         session_destroy();
-        header('Location: ../html/login.php');
-        exit();
-    }   
+    }
 }
+?>
