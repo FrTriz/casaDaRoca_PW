4c5e44b0d0296347c9ed1ffa16bb300b51b27e1b
JosephBM7 <johbr85@gmail.com>
2025-09-25 03:56:25 -0300

    Feat: Fiz pequenas alterações de lógica e estética para funcionar em todo o site, e agora mostra a quantidade de itens que tem no carrinho ao lado do icone do header.

--- html/cliente-header.php
+++ html/cliente-header.php
@@ -1,40 +1,40 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+// Garante que a sessão seja iniciada de forma centralizada e segura
+require_once __DIR__ . '/../php/session-manager.php';
+
+$num_itens_carrinho = isset($_SESSION['carrinho']) ? count($_SESSION['carrinho']) : 0;
 ?>
 <header class="main-header">
     <div class="container main-header-content">
         <div class="user-actions">
-            <a href="#" class="action-icon" id="user-icon" title="Login/Perfil">
+            <a href="" class="action-icon" id="user-icon" title="Login/Perfil">
                 <i class="fa-solid fa-user"></i>
             </a>
             <div id="user-modal" class="user-modal">
                 <div class="modal-content">
-
-                <?php if (isset($_SESSION['usuario_id'])): ?>
-                    
-                        <a href="cliente-perfil.php" class="modal-button">Perfil</a>
+                    <?php if (isset($_SESSION['usuario_id'])): ?>
+                        <a href="perfil.php" class="modal-button">Perfil</a>
                         <a href="../php/Funcoes/logout-login.php" class="modal-button">Logout</a>
-
-                    <?php else: // Se o usuário NÃO estiver logado ?>
-
+                    <?php else: ?>
                         <a href="login.php" class="modal-button">Login</a>
                         <a href="cadastro.php" class="modal-button">Cadastro</a>
-
                     <?php endif; ?>
-
                 </div>
             </div>
-            <a href="carrinho.php" class="action-icon" title="Carrinho de Compras">
+
+            <a href="carrinho.php" class="action-icon cart-icon-container" title="Carrinho de Compras">
                 <i class="fa-solid fa-cart-shopping"></i>
+                <?php if ($num_itens_carrinho > 0): ?>
+                    <span class="cart-badge"><?php echo $num_itens_carrinho; ?></span>
+                <?php endif; ?>
             </a>
-            <div class="search-box">
-                <input type="text" placeholder="Pesquisar...">
+            
+            <form action="pesquisa.php" method="GET" class="search-box">
+                <input type="text" name="q" placeholder="Pesquisar..." required>
                 <button type="submit">
                     <i class="fa-solid fa-magnifying-glass"></i>
                 </button>
-            </div>
+            </form>
         </div>
         <a href="index.php" class="logo" title="Ir para a página inicial">
             <img src="../img/logo.png" alt="Logo da Casa da Roça">
