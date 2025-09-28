9c1f6bbf69bc1bba4b4ada13672685da337fb2e4
FrTriz <beatrizfreitas314.bf@gmail.com>
2025-09-28 15:40:42 -0300

    ...

--- php/conexao.php
+++ php/conexao.php
@@ -18,9 +18,9 @@ $pass_com_endpoint = !empty($endpoint_id) ? "endpoint=$endpoint_id;" . $pass : $
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
 
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARESn => false, 
+    PDO::ATTR_EMULATE_PREPARES   => false, 
 ];
 
 try {
